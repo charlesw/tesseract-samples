62b5b43239e64a2af2c9e18898b514714e4aef53
Charles Weld <charles.weld@gmail.com>
2016-01-06 20:58:26 +1100

    Added src for tesseract samples

---
+++ Readme.md
@@ -0,0 +1,30 @@
+Samples that demonstrate using Tesseract from .net.
+
+## ConsoleDemo
+
+A short demo that ocr's an image and prints out the results using both Page.GetText()
+and a result iterator.
+
+## Web Demo
+
+A simple demonstration of using Tesseract from within ASP.NET.
+
+**Warning:** To keep things simple the sample will create a new instance of the TesseractEngine
+each time a image is processed. However this is not performant as creating a new TesseractEngine
+is expensive and would be a good candiate for pooling to allow a single engine instance to be
+reused.
+
+## License
+
+Copyright 2012-2015 Charles Weld.
+
+Licensed under the [Apache License, Version 2.0][apache2] (the "License"); you
+may not use this software except in compliance with the License. You may obtain
+a copy of the License at:
+
+[http://www.apache.org/licenses/LICENSE-2.0][apache2]
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
---
+++ src/Tesseract.ConsoleDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tesseract.ConsoleDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var testImagePath = "./phototest.tif";
+            if (args.Length > 0)
+            {
+                testImagePath = args[0];
+            }
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+                            Console.WriteLine("Text (GetText): \r\n{0}", text);
+                            Console.WriteLine("Text (iterator):");
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+
+                                do
+                                {
+                                    do
+                                    {
+                                        do
+                                        {
+                                            do
+                                            {
+                                                if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                                {
+                                                    Console.WriteLine("<BLOCK>");
+                                                }
+
+                                                Console.Write(iter.GetText(PageIteratorLevel.Word));
+                                                Console.Write(" ");
+
+                                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                                {
+                                                    Console.WriteLine();
+                                                }
+                                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                                            {
+                                                Console.WriteLine();
+                                            }
+                                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                                } while (iter.Next(PageIteratorLevel.Block));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
